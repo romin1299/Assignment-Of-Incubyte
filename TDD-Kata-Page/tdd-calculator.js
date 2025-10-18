e2360d1ee995b14f3d011d118ba82095dbeaacea
Romin <romin301.osl@gmail.com>
2025-10-18 12:03:39 +0530

    run - support comma-separated two numbers as string

--- TDD-Kata-Page/tdd-calculator.js
+++ TDD-Kata-Page/tdd-calculator.js
@@ -1,6 +1,12 @@
 const add = (numberAsString) => {
   if (!numberAsString) return 0;
-  return Number(numberAsString);
+
+  const splitWithCommaSeprator = numberAsString?.split(",");
+  const summationOfString = splitWithCommaSeprator?.reduce(
+    (acc, n) => Number(acc) + Number(n),
+    0
+  );
+  return Number(summationOfString);
 };
 
 module.exports = { add };
