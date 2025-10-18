78e8c2ea1767eef2ab7c80bc87b22d628d27d25d
Romin <romin301.osl@gmail.com>
2025-10-18 11:48:46 +0530

    test - pass 1 as string failed return undefined

--- TDD-Test-Suits/tdd-calculator.test.js
+++ TDD-Test-Suits/tdd-calculator.test.js
@@ -4,4 +4,8 @@ describe("String calculator of TDD", () => {
   test("empty string return 0", () => {
     expect(add("")).toBe(0);
   });
+
+  test("one string number to return 1", () => {
+    expect(add("1")).toBe(1);
+  });
 });
